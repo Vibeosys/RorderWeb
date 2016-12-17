1b2884408602fab767802c34d49b73ef9cbbc09a
anandvb <anand@vibeosys.com>
2016-12-17 20:48:17 +0530

    Resolved menu category issue
    Resolved issue related to addition of menu category

--- src/Controller/MenuCategoryController.php
+++ src/Controller/MenuCategoryController.php
@@ -20,11 +20,10 @@ use Cake\Filesystem\Folder;
  */
 define('MC_INS_QRY', "INSERT INTO menu_category (CategoryId,CategoryTitle,CategoryImage,Active,Colour)"
         . " VALUES (@CategoryId,\"@CategoryTitle\",\"@CategoryImage\",@Active,\"@Colour\");");
-define('CATE_IMG_PATH',TMP.'menu_category_img'.DS);
+define('CATE_IMG_PATH', TMP . 'menu_category_img' . DS);
+
 class MenuCategoryController extends ApiController {
-    
-    
-    
+
     private function getTableObj() {
 
         return new Table\MenuCategoryTable();
@@ -57,88 +56,84 @@ class MenuCategoryController extends ApiController {
     }
 
     public function addNewMenuCategory() {
-         if(!$this->isLogin()){
+        if (!$this->isLogin()) {
             $this->redirect('login');
         }
-         $data = $this->request->data();
-         $counter = 0;
+        $data = $this->request->data();
+        $counter = 0;
         if ($this->request->is('post') and isset($this->request->data['bulk'])) {
-           
-            $file = $data['file-upload']['tmp_name'];
-            $extenstion = $this->getExtension($data['file-upload']['name']);
-            if (empty($file)) {
-                $this->set([MESSAGE => SELECT_FILE_MESSAGE, 'color' => 'red', 'bulk' => 1]);
-            } elseif ($extenstion != CSV_EXT) {
-                Log::debug('File extention :-'.$file);
-                $this->set([MESSAGE => INCORRECT_FILE_MESSAGE, 'color' => 'red','bulk' => 1]);
-            } else {
-                if (($handle = fopen($file, "r")) !== FALSE) {
-                    
-                    $allMenuCategory = null;
-                    fgetcsv($handle);
-                    while (($filesop = fgetcsv($handle, 1000, ",")) !== false) {
-                        $allMenuCategory[$counter] = new DownloadDTO\MenuCategoryDownloadDto(
-                                null, 
-                                $filesop[0], 
-                                $filesop[1], 
-                                ACTIVE, 
-                                $filesop[2]);
-                        $counter++;
-                    }
-                    fclose($handle);
-                    $result = $this->getTableObj()->insert($allMenuCategory);
+            //If the file is readable by the program
+            if (($handle = fopen($file, "r")) !== FALSE) {
+                $allMenuCategory = null;
+                fgetcsv($handle);
+                while (($filesop = fgetcsv($handle, 1000, ",")) !== false) {
+                    $allMenuCategory[$counter] = new DownloadDTO\MenuCategoryDownloadDto(
+                            null, $filesop[0], NULL, ACTIVE, $filesop[2]);
+                    $counter++;
                 }
-                if ($result) {
-                    $this->set([MESSAGE => 'You database has imported successfully. You have inserted ' . $result . ' recoreds', 'color' => 'green','bulk' => 1]);
-                } else {
-                    $this->set([MESSAGE => DB_FILE_ERROR, 'color' => 'red','bulk' => 1]);
-                }
-            }
-        }elseif ($this->request->is('post') and isset($this->request->data['single'])) {
-            $file = $data['image']['tmp_name'];
-            $fileName = $data['image']['name'];
-            $dir = new Folder(CATE_IMG_PATH, TRUE);
-            $destination = $dir->path.$fileName;
-            $extenstion = $this->getExtension($fileName);
-            if (empty($file)) {
-                $this->set([MESSAGE => SELECT_FILE_MESSAGE, 'color' => 'red', 'single' => 1]);
-            } elseif (!in_array($extenstion, $this->img_valid_ext)) {
-                Log::debug('File extention :-'.$file);
-                $this->set([MESSAGE => INCORRECT_FILE_MESSAGE, 'color' => 'red','single' => 1]);
-            } elseif(move_uploaded_file($file, $destination)) {
-                $allMenuCategory[$counter] = new DownloadDTO\MenuCategoryDownloadDto(
-                                null, 
-                                $data['title'], 
-                                $destination, 
-                                ACTIVE, 
-                                null);
+                fclose($handle);
                 $result = $this->getTableObj()->insert($allMenuCategory);
-                 if ($result) {
-                    $this->redirect('menucategory');
-                } else {
-                    $this->set([MESSAGE => 'Error in image upload ! please try again.', 'color' => 'red','single' => 1]);
-                }
-            }else{
-                $this->set([MESSAGE => 'Error in image upload ! please try again.', 'color' => 'red','single' => 1]);
+            }
+            //If the insertion in tables is successful
+            if ($result) {
+                $this->set([MESSAGE => 'You CSV file imported successfully. You have inserted ' . $result . ' recoreds',
+                    'color' => 'green', 'bulk' => 1]);
+            } else {
+                $this->set([MESSAGE => DB_FILE_ERROR, 'color' => 'red', 'bulk' => 1]);
+            }
+        } elseif ($this->request->is('post') and isset($this->request->data['single'])) {
+            //Upload file if not empty
+            $destination = $this->uploadSingleImageFile($data);
+            //Add menu category in database
+            $allMenuCategory[$counter] = new DownloadDTO\MenuCategoryDownloadDto(
+                    null, $data['title'], $destination, ACTIVE, null);
+            $result = $this->getTableObj()->insert($allMenuCategory);
+            //Check if insertion is successful
+            if ($result) {
+                $this->redirect('menucategory');
+            } else {
+                $this->set([MESSAGE => 'Error while adding menu category ! please try again.', 'color' => 'red', 'single' => 1]);
             }
         }
         Log::debug('Cancel button pressed from menu category');
     }
-    
+
+    private function uploadSingleImageFile($data) {
+        $file = $data['image']['tmp_name'];
+        $fileName = $data['image']['name'];
+        $dir = new Folder(CATE_IMG_PATH, TRUE);
+        $destination = $dir->path . $fileName;
+        $extenstion = $this->getExtension($fileName);
+        $resultantFile = "";
+        //If file not empty then do the following else escape
+        if (!empty($file)) {
+            if (!in_array($extenstion, $this->img_valid_ext)) {
+                Log::debug('File extention :-' . $file);
+                $this->set([MESSAGE => INCORRECT_FILE_MESSAGE, 'color' => 'red', 'single' => 1]);
+            } elseif (move_uploaded_file($file, $destination)) {
+                $resultantFile = $destination;
+            }else {
+                $this->set([MESSAGE => 'Error in image upload ! please try again.', 'color' => 'red', 'single' => 1]);
+            }
+        }
+        return $resultantFile;
+    }
+
     public function getStdMenuCategory() {
         $allMenuCategory = $this->getMenuCategories();
-        if($allMenuCategory){
-             $mCategory = new \stdClass();
-            foreach ($allMenuCategory as $category){
+        if ($allMenuCategory) {
+            $mCategory = new \stdClass();
+            foreach ($allMenuCategory as $category) {
                 $key = $category->categoryId;
                 $mCategory->$key = $category->categoryTitle;
             }
-            return $mCategory;  
+            return $mCategory;
         }
         return FALSE;
     }
+
     public function menuCategoryList() {
-        if(!$this->isLogin()){
+        if (!$this->isLogin()) {
             $this->redirect('login');
         }
         $menuCategories = $this->getTableObj()->getMenuCategory();
@@ -146,7 +141,5 @@ class MenuCategoryController extends ApiController {
             'menu_cate' => $menuCategories
         ]);
     }
-    
-    
 
 }
--- src/Template/MenuCategory/add_new_menu_category.ctp
+++ src/Template/MenuCategory/add_new_menu_category.ctp
@@ -44,7 +44,7 @@ $this->assign('heading', 'Add New Menu Category');
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Image 
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="file" name="image" id="image" class="inputfile inputfile-6 date-picker" required="required"  />
+                                <input type="file" name="image" id="image" class="inputfile inputfile-6 date-picker" />
                                 <label for="image"><span></span> <strong><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> Choose a file&hellip;</strong></label>
                                 <br> (Please choose image file)
                                     <?php if (isset($single)) { ?>
